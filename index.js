a4dd1a8c006ed30ca5eacbe70009de3119f0517c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-29 15:28:39 +0530

    creat basic login api

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ app.use(express.json());
 import dotenv from "dotenv";
 dotenv.config();
 
+const screatkey="secreatkey"
+
 app.get('/health',(req, res)=>{
     res.json({
         success: true,
         message: "hiiiii"
+    })
+})
+
+
+app.post('/login',(req, res)=>{
+    const user ={
+        id:1,
+        username: "Anand",
+        email: "anand@gmail.com"
+    }
+    jwt.sign({user},screatkey, {expiresIn: '300s'}, (err, token)=>{
+        res.json({
+            token
+        })
     })
 })
 
